8b40dd26c545665c0ec1b74de0cf736a5a435256
lvr <lvrui_ah@bonc.com.cn>
2022-12-02 22:54:24 +0800

    feat: 分类模块完成

--- router/goods.js
+++ router/goods.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const expressJoi = require('@escook/express-joi')
 
 //导入路由处理模块
-const { getCategoryList,addCategory } = require('../router_handler/goods')
+const { getCategoryList,addCategory,deleteCategory,getCategoryById,updateCategory } = require('../router_handler/goods')
 
-const { add_cate_schema } = require('../schema/goods')
+const { add_cate_schema,delete_cate_schema,update_cate_schema } = require('../schema/goods')
 
 
 //获取物品分类列表
@@ -16,5 +16,14 @@ router.get('/categoryList', getCategoryList)
 
 //新增分类
 router.post('/categoryAdd', expressJoi(add_cate_schema), addCategory)
+
+//删除分类
+router.get('/categoryDel/:id', expressJoi(delete_cate_schema), deleteCategory)
+
+//根据id查询
+router.get('/categoryById/:id', expressJoi(delete_cate_schema), getCategoryById)
+
+//更新分类信息
+router.post('/categoryUpdate/:id', expressJoi(update_cate_schema), updateCategory)
 
 module.exports = router;
--- router_handler/goods.js
+++ router_handler/goods.js
@@ -11,7 +11,7 @@ exports.getCategoryList = (req, res) => {
         // 2. 执行 SQL 语句成功
         res.send({
             status: 0,
-            message: '获取文章分类列表成功！',
+            message: '获取分类列表成功！',
             data: results,
         })
     })
@@ -49,6 +49,68 @@ exports.addCategory = (req, res) => {
             res.cc('新增分类成功！', 0)
         })
 
+
+    })
+
+}
+
+//删除分类
+exports.deleteCategory = (req, res) => {
+    const sql = `update category set status=0 where id=?`
+    db.query(sql, req.params.id, (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+
+        // SQL 语句执行成功，但是影响行数不等于 1
+        if (results.affectedRows !== 1) return res.cc('删除分类失败！')
+
+        // 删除文章分类成功
+        res.cc('删除分类成功！', 0)
+    })
+
+}
+
+//根据id查询分类
+exports.getCategoryById = (req, res) => {
+    const sql = "select name, alias, parent_id as parentId from category where id=? and status = 1"
+    db.query(sql, req.params.id, (err, rows) => {
+        if (err) return res.cc(err)
+        res.send({
+            status: 0,
+            message: "获取分类信息成功！",
+            data: rows[0],
+        })
+    })
+}
+
+//更新分类信息
+exports.updateCategory = (req, res) => {
+    // 定义查询 分类名称 与 分类别名 是否被占用的 SQL 语句
+    const sql = `select * from category where id = ? and (name=? or alias=?)`
+    // 执行查重操作
+    db.query(sql, [req.params.id, req.body.name, req.body.alias], (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+
+        // 分类名称 和 分类别名 都被占用
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！')
+        // 分类名称 或 分类别名 被占用
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！')
+
+        // TODO：更新文章分类
+        const sql2 = `update category set ? where id=?`
+        db.query(sql2, [req.body, req.params.id], (err, results) => {
+            // 执行 SQL 语句失败
+            if (err) return res.cc(err)
+
+            // SQL 语句执行成功，但是影响行数不等于 1
+            if (results.affectedRows !== 1) return res.cc('更新分类失败！')
+
+            // 更新文章分类成功
+            res.cc('更新分类成功！', 0)
+        })
 
     })
 
